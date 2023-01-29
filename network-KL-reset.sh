a28da54c20192dbac4f0a1af4e7147e2f01ddfb1
baldwinT12 <123429443+baldwinT12@users.noreply.github.com>
2023-01-28 21:38:44 -0500

    Add files via upload

---
+++ network-KL-reset.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+
+'''
+Author : baldwinT12
+Start Date : 2023/JAN
+'''
+
+
+sudo service network-manager restart
+sudo dhclient
+sudo ifconfig
+echo "baldwinT12 said "Your Network Interface Has Been Reset!"
